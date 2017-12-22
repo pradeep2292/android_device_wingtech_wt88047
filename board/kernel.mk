cfbc2a1fafbd40422f2a17a6ed7c01c1ddbf0cdc
Pradeep Varma <pradeepvarma107@gmail.com>
2017-12-22 23:27:55 +0530

    Update kernel.mk

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,6 +1,6 @@
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
-TARGET_KERNEL_CONFIG := cyanogenmod_wt88047_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/gucci
+TARGET_KERNEL_CONFIG := cyanogenmod_gucci_defconfig
 
 BOARD_KERNEL_CMDLINE += \
     sched_enable_hmp=1 \
